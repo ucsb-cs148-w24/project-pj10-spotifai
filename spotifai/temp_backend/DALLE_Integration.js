508591c4b2b565b97309f54cd1084179934029a2
kyleuwaine <kyleuwaine@gmail.com>
2024-02-05 14:15:52 -0800

    fix syntax

--- spotifai/temp_backend/DALLE_Integration.js
+++ spotifai/temp_backend/DALLE_Integration.js
@@ -1,9 +1,9 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI(api_key = "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");
+const openai = new OpenAI("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");
 
 async function generate_cover(prompt) {
-    const response = await openai.createImage({
+    const response = await openai.images.generate({
         model: "dall-e-3",
         prompt: prompt,
         n: 1,
