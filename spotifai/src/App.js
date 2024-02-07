957787389fa8107eac9307e06028019752edf3e5
Richard Fang <richardfang888@gmail.com>
2024-02-07 02:35:03 -0800

    added initial playlist and sidebar components (assuming working Spotify API auth)

--- spotifai/src/App.js
+++ spotifai/src/App.js
@@ -1,8 +1,22 @@
-import logo from './logo.svg';
 import WorldMapChart from './components/DemographicMap.jsx';
+import { reducerCases } from "./utils/Constants";
+import { useStateProvider } from "./utils/StateProvider";
 import './App.css';
 
 function App() {
+  const [{ token }, dispatch] = useStateProvider();
+  
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      if (token) {
+        dispatch({ type: reducerCases.SET_TOKEN, token });
+      }
+    }
+    document.title = "Spotify";
+  }, [dispatch, token]);
+
   return (
     <div className="App">
       <div style = {{
