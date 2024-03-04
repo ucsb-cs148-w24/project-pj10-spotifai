8e6ceac91d68c24340c003326b2ec3fcff4d3007
Richard Fang <richardfang888@gmail.com>
2024-03-04 04:56:41 -0800

    unsticky header row

--- spotifai/src/components/Body.jsx
+++ spotifai/src/components/Body.jsx
@@ -218,7 +218,6 @@ const Container = styled.div`
       grid-template-columns: 0.3fr 3fr 2fr 0.1fr;
       margin: 1rem 0 0 0;
       color: #dddcdc;
-      position: relative;
       top: 15vh;
       padding: 1rem 3rem;
       transition: 0.3s ease-in-out;
