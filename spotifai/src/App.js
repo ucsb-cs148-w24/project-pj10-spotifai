640cb1e4a28224298bb09d1454b1519c7a8d948e
Your Name <gracefeng@ucsb.edu>
2024-03-03 23:07:14 -0800

    Commit

--- spotifai/src/App.js
+++ spotifai/src/App.js
@@ -1,7 +1,6 @@
 import WorldMapChart from "./components/DemographicMap.jsx";
 import "./App.css";
 import PlaylistCoverGenerator from "./components/cover/PlaylistCover.jsx";
-import BeatVisualizer from "./components/beat_visualizer/BeatVisualizer.js";
 import LyricAnalysis from "./components/lyric_analysis/LyricAnalysis.js";
 import React, { useEffect } from "react";
 import Login from "./components/Login.jsx";
